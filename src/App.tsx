2fa9bfdd250319b2c61f4cc3d9a8d55077fecfc9
MOUAAD-NASSIH <mouadnassih11@gmail.com>
2024-08-14 19:21:47 +0100

    add the Workflow section

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import Feature from "./components/Feature";
+import Workflow from "./components/Workflow";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <div className="container mx-auto pt-20 px-6">
         <Hero />
         <Feature />
+        <Workflow />
       </div>
     </>
   );
