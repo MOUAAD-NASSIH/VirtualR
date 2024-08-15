b30f00280ca505cf92f95c1aea244dff0fec9aa3
MOUAAD-NASSIH <mouadnassih11@gmail.com>
2024-08-15 04:47:50 +0100

    add the Testimonials section

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar";
 import Feature from "./components/Feature";
 import Workflow from "./components/Workflow";
 import Pricing from "./components/Pricing";
+import Testimonials from "./components/Testimonials";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Feature />
         <Workflow />
         <Pricing />
+        <Testimonials />
       </div>
     </>
   );
